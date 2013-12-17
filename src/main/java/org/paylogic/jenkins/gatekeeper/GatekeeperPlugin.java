4e429c441f2f374d605a2a7eb2ce794060e16b5f
Maikel Wever <maikelwever@gmail.com>
2013-12-17 17:33:12 +0100

    Prepare GatekeeperPlugin to fetch code from developer repo with gatekeepered revision ID from modified rietveld.

--- src/main/java/org/paylogic/jenkins/gatekeeper/GatekeeperPlugin.java
+++ src/main/java/org/paylogic/jenkins/gatekeeper/GatekeeperPlugin.java
@@ -11,14 +11,19 @@ import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
 import lombok.Getter;
 import lombok.extern.java.Log;
+import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 import org.paylogic.fogbugz.FogbugzCase;
 import org.paylogic.jenkins.advancedmercurial.AdvancedMercurialManager;
 import org.paylogic.jenkins.fogbugz.FogbugzNotifier;
 import org.paylogic.redis.RedisProvider;
 import redis.clients.jedis.Jedis;
 
+import javax.swing.text.html.HTML;
 import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.logging.Level;
 
 
@@ -61,13 +66,36 @@ public class GatekeeperPlugin extends Builder {
 
         /* Fetch branch information from Fogbugz */
         FogbugzCase fallbackCase = new FogbugzNotifier().getFogbugzCaseManager().getCaseById(usableCaseId);
+        String repo_path = fallbackCase.getFeatureBranch().split("#")[0];
         String featureBranch = fallbackCase.getFeatureBranch().split("#")[1];
         String targetBranch = fallbackCase.getTargetBranch();
 
-        /* Actual Gatekeepering logic. */
-        amm.pull();
-        amm.update(targetBranch);
-        amm.mergeWorkspaceWith(featureBranch);
+        /* Actual Gatekeepering logic. Seperated to work differently when Rietveld support is active. */
+        boolean runNormalMerge = this.getDescriptor().getUrl().isEmpty();
+        if (runNormalMerge) {
+            // Use Rietveld support.
+            String featureRepoUrl = this.getDescriptor().getRepoBase() + repo_path;  // Should produce correct url.
+
+            URL uri = new URL(this.getDescriptor().getUrl() + "/get_latest_ok_for_case/" + Integer.toString(usableCaseId)  + "/");
+            HttpURLConnection con = (HttpURLConnection) uri.openConnection();
+            if (con.getResponseCode() != 200) {
+                log.log(Level.SEVERE, "Error while fetching latest OK revision from Rietveld.");
+                return false;
+            }
+
+            String okRevision = con.getInputStream().toString();
+            amm.pull(featureRepoUrl);
+            amm.update(targetBranch);
+            amm.mergeWorkspaceWith(okRevision);
+        }
+
+        if (!runNormalMerge) {  // We check twice, so on failure we should be able to resume normally (not in use atm.)
+            amm.pull();
+            amm.update(targetBranch);
+            amm.mergeWorkspaceWith(featureBranch);
+        }
+
+
         amm.commit("[Jenkins Integration Merge] Merge " + targetBranch + " with " + featureBranch);
 
         /* Set the featureBranch we merged with in redis to other plugin know this was actually the build's branch */
@@ -89,6 +117,41 @@ public class GatekeeperPlugin extends Builder {
 
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        private String url;
+        private String repoBase;
+
+        public String getUrl() {
+            if (this.url == null) {
+                return "";
+            } else {
+                return this.url;
+            }
+        }
+
+        public void setUrl(String url) {
+            if (url == null) {
+                this.url = "";
+            } else {
+                this.url = url;
+            }
+        }
+
+        public String getRepoBase() {
+            if (this.repoBase == null) {
+                return "";
+            } else {
+                return this.repoBase;
+            }
+        }
+
+        public void setRepoBase(String repoBase){
+            if (repoBase == null) {
+                this.repoBase = "";
+            } else {
+                this.repoBase = repoBase;
+            }
+        }
+
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> jobType) {
             return true;
@@ -97,6 +160,19 @@ public class GatekeeperPlugin extends Builder {
         @Override
         public String getDisplayName() {
             return "Perform Gatekeerping.";
+        }
+
+        public DescriptorImpl() {
+            super();
+            load();
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            this.setUrl(formData.getString("url"));
+            this.setRepoBase(formData.getString("repoBase"));
+            save();
+            return super.configure(req, formData);
         }
     }
 }
