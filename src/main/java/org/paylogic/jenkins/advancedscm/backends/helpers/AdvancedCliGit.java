42f3cd4a4ea247dd16af22051c092aaadf37da91
Anatoly Bubenkov <bubenkoff@gmail.com>
2014-07-15 07:43:06 +0200

    set launcher for git tool

--- src/main/java/org/paylogic/jenkins/advancedscm/backends/helpers/AdvancedCliGit.java
+++ src/main/java/org/paylogic/jenkins/advancedscm/backends/helpers/AdvancedCliGit.java
@@ -2,23 +2,30 @@ package org.paylogic.jenkins.advancedscm.backends.helpers;
 
 import hudson.EnvVars;
 import hudson.Launcher;
-import hudson.model.TaskListener;
 import hudson.model.Node;
+import hudson.model.TaskListener;
 import hudson.plugins.git.GitSCM;
 import org.jenkinsci.plugins.gitclient.CliGitAPIImpl;
 
 import java.io.File;
+import java.lang.reflect.Field;
 
 /**
  * Created by bubenkoff on 4/22/14.
  */
 public class AdvancedCliGit extends CliGitAPIImpl{
 
-    protected transient Launcher launcher;
-
     public AdvancedCliGit(GitSCM scm, Launcher launcher, Node node, File workspace,
                             TaskListener listener, EnvVars environment) {
         super(scm.getGitExe(node, environment, listener), workspace, listener, environment);
-        this.launcher = launcher;
+        try {
+            Field field = CliGitAPIImpl.class.getDeclaredField("launcher");
+            field.setAccessible(true);
+            field.set(this, launcher);
+        } catch (NoSuchFieldException exception) {
+        }
+        catch (IllegalAccessException exception) {
+            
+        }
     }
 }
